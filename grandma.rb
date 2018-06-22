8b7ac48e1a07da402a8f640f62a064a4af963c15
Jon Schulman <schulman99@gmail.com>
2018-06-22 18:57:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ def speak_to_grandma(phrase)
   elsif  phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else
-  //if phrase == phrase.upcase
+  #if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
 end
